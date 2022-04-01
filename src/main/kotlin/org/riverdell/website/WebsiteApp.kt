b676dba93986067470740882ed205eff43e299e2
GrowlyX <growlyxdev@gmail.com>
2022-04-01 15:59:52 -0400

    Remove example PWAs & update conf

--- src/main/kotlin/org/riverdell/website/WebsiteApp.kt
+++ src/main/kotlin/org/riverdell/website/WebsiteApp.kt
@@ -2,13 +2,14 @@ package org.riverdell.website
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 /**
  * @author GrowlyX
  * @since 3/30/2022
  */
 @SpringBootApplication
-open class WebsiteApp
+open class WebsiteApp : SpringBootServletInitializer()
 {
     companion object
     {
--- src/main/kotlin/org/riverdell/website/views/PrimaryView.kt
+++ src/main/kotlin/org/riverdell/website/views/PrimaryView.kt
@@ -1,14 +1,13 @@
 package org.riverdell.website.views
 
-import com.example.demo.GreetService
-import com.github.mvysny.karibudsl.v10.*
+import com.github.mvysny.karibudsl.v10.KComposite
+import com.github.mvysny.karibudsl.v10.button
+import com.github.mvysny.karibudsl.v10.horizontalLayout
+import com.github.mvysny.karibudsl.v10.onLeftClick
 import com.vaadin.flow.component.Key
-import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.button.ButtonVariant
 import com.vaadin.flow.component.dependency.CssImport
 import com.vaadin.flow.component.notification.Notification
-import com.vaadin.flow.component.orderedlayout.VerticalLayout
-import com.vaadin.flow.component.textfield.TextField
 import com.vaadin.flow.router.Route
 
 /**
