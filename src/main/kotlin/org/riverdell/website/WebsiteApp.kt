a002fbb3633c636331b91358d3a88c834319748d
GrowlyX <growlyxdev@gmail.com>
2022-04-02 22:34:30 -0400

    Use cobalt's theme

--- src/main/kotlin/org/riverdell/website/WebsiteApp.kt
+++ src/main/kotlin/org/riverdell/website/WebsiteApp.kt
@@ -1,5 +1,8 @@
 package org.riverdell.website
 
+import com.vaadin.flow.component.dependency.NpmPackage
+import com.vaadin.flow.theme.Theme
+import com.vaadin.flow.theme.lumo.Lumo
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
@@ -8,6 +11,14 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * @author GrowlyX
  * @since 3/30/2022
  */
+@Theme(
+    value = "stolen-cobalt",
+    variant = Lumo.DARK
+)
+@NpmPackage(
+    value = "line-awesome",
+    version = "1.3.0"
+)
 @SpringBootApplication
 open class WebsiteApp : SpringBootServletInitializer()
 {
